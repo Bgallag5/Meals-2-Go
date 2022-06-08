d507c7f77f799c792b0b255db32dd6dac6ac3255
bengallagher <bgallag5@gmail.com>
2022-06-08 13:45:43 -0400

    scroll progress component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,70 +1,34 @@
-import React, {useContext, useEffect, useRef, useState} from "react";
+import React, { useContext } from "react";
 import { AppContext } from "../../App";
 import { GlobalContext } from "../../store/GlobalStore";
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import { faCartShopping, } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 import AppMessage from "../UI/AppMessage";
+import ScrollBar from '../UI/ScrollBar';
 
 export default function Header() {
-
-    const {handleToggleModal } = useContext(AppContext);
-    const {items, appMessage } = useContext(GlobalContext);
-
-    // const [currentScroll, setCurrentScroll] = useState(0);
-    const scrollBarRef = useRef('0%');
-    
-    
-    //get max window scroll
-    const handleScroll = () => {
-        const bodyEl = document.querySelector('.app-container')
-
-        //offset - height of viewport (height of what is on screen)
-        let offset = window.innerHeight;
-        //maxScroll - max we CAN scroll, so total body height - viewport height
-        let maxScroll = bodyEl.scrollHeight - offset;
-        //currentScroll - how far below top (0) we have scrolled
-        let currentScroll = window.scrollY;
-
-        if (currentScroll > 0){
-            scrollBarRef.current.style.visibility = 'visible'
-        }
-        if (currentScroll <= 0){
-            scrollBarRef.current.style.visibility = 'hidden'
-        }
-
-        // console.log(offset);
-        console.log( 'MAX SCROLL:', maxScroll);
-        console.log( 'CURRENT:', currentScroll);
-
-
-
-        //set width of scrollbar to currentScroll/maxScroll;
-        // let currentScroll = (maxScroll / window.scrollY);
-        console.dir(scrollBarRef);
-        const scrollValue = `${(currentScroll / maxScroll) * 100}%` ;
-        console.log(scrollValue);
-        scrollBarRef.current.style.width = scrollValue;
-
-
-    }
-    document.addEventListener('scroll', handleScroll);
-
-
+  const { handleToggleModal } = useContext(AppContext);
+  const { items, appMessage } = useContext(GlobalContext);
 
   return (
     <>
-    {appMessage?.msg && <AppMessage />}
+      {appMessage?.msg && <AppMessage />}
       <div className="header flex-col">
         <header className="header__main flex-row">
           <h1 className="header__main--title">Meals-2-Go</h1>
-          <button onClick={handleToggleModal} className="header__main--cart btn flex-row ">
-              <FontAwesomeIcon icon={faCartShopping} />
-            <p className="text-regular">Cart</p> 
-            <div className="header__main--cart--items flex-row">{items.reduce((acc, cur) => acc + cur.quantity, 0)}</div>
+          <button
+            onClick={handleToggleModal}
+            className="header__main--cart btn flex-row "
+          >
+            <FontAwesomeIcon icon={faCartShopping} />
+            <p className="text-regular">Cart</p>
+            <div className="header__main--cart--items flex-row">
+              {items.reduce((acc, cur) => acc + cur.quantity, 0)}
+            </div>
           </button>
         </header>
-          <div ref={scrollBarRef} className="header__scrollbar"></div>
+        <ScrollBar />
       </div>
       <div className="header__img"></div>
     </>
---
+++ src/components/UI/ScrollBar.js
@@ -0,0 +1,32 @@
+import React, { useRef } from "react";
+
+export default function ScrollBar() {
+  const scrollBarRef = useRef("0%");
+  //get max window scroll
+  const handleScroll = () => {
+    const bodyEl = document.querySelector(".app-container");
+
+    //offset - height of viewport (height of what is on screen)
+    let offset = window.innerHeight;
+    //maxScroll - max we CAN scroll, so total body height - viewport height
+    let maxScroll = bodyEl.scrollHeight - offset;
+    //currentScroll - how far below top (0) we have scrolled
+    let currentScroll = window.scrollY;
+
+    //toggle scrollbar visibility if currentScroll is below 0
+    if (currentScroll > 0) {
+      scrollBarRef.current.style.visibility = "visible";
+    }
+    if (currentScroll <= 0) {
+      scrollBarRef.current.style.visibility = "hidden";
+    }
+
+    //set width of scrollbar to currentScroll/maxScroll;
+    const scrollValue = `${(currentScroll / maxScroll) * 100}%`;
+    scrollBarRef.current.style.width = scrollValue;
+  };
+  //listen for scroll
+  document.addEventListener("scroll", handleScroll);
+
+  return <div ref={scrollBarRef} className="header__scrollbar"></div>;
+}
