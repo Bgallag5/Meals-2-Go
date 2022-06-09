1c84231f28436145bb05f415ca126d811758d11b
bengallagher <bgallag5@gmail.com>
2022-06-09 16:37:45 -0400

    email fixes

--- src/components/Layout/Email.js
+++ src/components/Layout/Email.js
@@ -18,7 +18,7 @@ export const Email = () => {
   return (
     <form ref={form} onSubmit={sendEmail}>
       <label>Name</label>
-      <input type="text" name="user_name" />
+      <input type="text" name="to_name" />
       <label>Email</label>
       <input type="email" name="user_email" />
       <label>Message</label>
